182577c89360220cb30f709b2964ba0b249ad85b
LucasAdao <l.adao@outlook.com>
2024-08-01 22:01:42 -0300

    Atualizando Palavras

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const mensagem = document.querySelector(".textarea-recifense")
 
 function encriptar(stringEcriptada){
 
-    let palavrasRecife = [["jogar futebol","bater uma pelada"] ,["vou embora","vou chegar"],["mentira","pala"],["muito bom","do carai"],["triste","na magua"],["melhor comida","cuscuz"],["lerdo","donzelo"],["namorada","boysinha"],["com fome","na larica"],["classe media","play boy"],["muita chuva","toro do karai"],["sol se por","sol esfriar"]];
+    let palavrasRecife = [["jogar futebol","bater uma pelada"] ,["vou embora","vou chegar"],["mentira","pala"],["muito bom","do carai"],["triste","na magua"],["melhor comida","cuscuz"],["lerdo","donzelo"],["namorada","boysinha"],["com fome","na larica"],["classe media","play boy"],["muita chuva","toro do karai"],["sol se por","sol esfriar"],["amigo próximo", "chegado"],["com calma", "na manha"], ["corrida rápido", "carreira do caramba"],["ficou com raiva", "ta arretado"], ["muito bom", "massa"], ["parar a brincadeira", "dispense"] ];
     stringEcriptada = stringEcriptada.toLowerCase();
 
     for(let i = 0; i < palavrasRecife.length; i++){
@@ -22,7 +22,7 @@ function btnEncriptar(){
 
 function desencriptar(stringDesencriptada){
 
-    let palavrasRecife = [["jogar futebol","bater uma pelada"] ,["vou embora","vou chegar"],["mentira","pala"],["muito bom","do carai"],["triste","na magua"],["melhor comida","cuscuz"],["lerdo","donzelo"],["namorada","boysinha"],["com fome","na larica"],["classe media","play boy"],["muita chuva","toro do karai"],["sol se por","sol esfriar"],["amigo próximo", "chegado"],["com calma", "na manha"], ["corrida rápido", "carreira do caramba"],["ficou com raiva", ["ta arretado"]], ["muito bom", "massa"], ["parar a brincadeira", "dispense"] ];
+    let palavrasRecife = [["jogar futebol","bater uma pelada"] ,["vou embora","vou chegar"],["mentira","pala"],["muito bom","do carai"],["triste","na magua"],["melhor comida","cuscuz"],["lerdo","donzelo"],["namorada","boysinha"],["com fome","na larica"],["classe media","play boy"],["muita chuva","toro do karai"],["sol se por","sol esfriar"],["amigo próximo", "chegado"],["com calma", "na manha"], ["corrida rápido", "carreira do caramba"],["ficou com raiva", "ta arretado"], ["muito bom", "massa"], ["parar a brincadeira", "dispense"] ];
     stringDesencriptada = stringDesencriptada.toLowerCase();
 
     for(let i = 0; i < palavrasRecife.length; i++){
